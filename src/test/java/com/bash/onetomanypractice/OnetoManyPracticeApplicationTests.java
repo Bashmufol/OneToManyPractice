e7216d40d6737214233cc5e2d969c7596368e66b
Bashir Muhammed <bashmufol@gmail.com>
2025-08-09 17:49:17 +0100

    first commit

---
+++ src/test/java/com/bash/onetomanypractice/OnetoManyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.onetomanypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetoManyPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
