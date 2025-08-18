699f85f8d0d3af6cacd79c07cb4de96a802413d1
Muhammed Bashir <bashmufol@gmail.com>
2025-08-18 06:23:24 +0100

    updated

--- src/test/java/com/bash/onetomanypractice/CategoryRepoTests.java
+++ src/test/java/com/bash/onetomanypractice/CategoryRepoTests.java
@@ -1,9 +1,26 @@
 package com.bash.onetomanypractice;
 
+import com.bash.onetomanypractice.model.Categories;
+import com.bash.onetomanypractice.repository.CategoryRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CategoryRepoTests {
+
+    @Autowired
+    private CategoryRepo categoryRepo;
+
+    @Test
+    public void testCategoryRepo() {
+        Categories category = new Categories();
+        Categories savedCategories = categoryRepo.save(category);
+        assertThat(savedCategories.getId()).isGreaterThan(0);
+
+    }
 }
