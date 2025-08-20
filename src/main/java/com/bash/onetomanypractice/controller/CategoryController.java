418d9f56b62dbceb2a151d242ba105f6b1524ec2
Muhammed Bashir <bashmufol@gmail.com>
2025-08-20 04:53:59 +0100

    updated

--- src/main/java/com/bash/onetomanypractice/controller/CategoryController.java
+++ src/main/java/com/bash/onetomanypractice/controller/CategoryController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -28,5 +30,11 @@ public class CategoryController {
     public String showNewCategoryForm(Model model) {
         model.addAttribute("category", new Category());
         return "category_form";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory(@ModelAttribute("category") Category category) {
+        categoryRepo.save(category);
+        return "redirect:/categories";
     }
 }
---
+++ src/main/java/com/bash/onetomanypractice/model/Product.java
@@ -0,0 +1,11 @@
+package com.bash.onetomanypractice.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product {
+}
